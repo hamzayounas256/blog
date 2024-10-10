0c4b117e0957e74fee9e592a93370cd833506031
Hamza Younas <deadbody009@gmail.com>
2024-10-10 11:26:04 +0500

    update login

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -1,29 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
-
-// export const register = (req, res) => {
-// 	// Check existing user
-
-// 	const q = "SELECT * FROM users WHERE email  = ? OR username = ?";
-// 	db.query(q, [req.body.email, req.body.username], (err, data) => {
-// 		// console.log("Request body:", req.body);
-// 		if (err) return res.status(500).json(err);
-// 		if (data.length) return res.status(409).json("User already exists");
-
-// 		// Hash to password
-// 		const salt = bcrypt.genSaltSync(10);
-// 		const hash = bcrypt.hashSync(req.body.password, salt);
-
-// 		//Create a new user
-// 		const q = "INSERT INTO users (`username`, `email`, `password`) VALUES (?)";
-// 		const values = [req.body.username, req.body.email, hash];
-
-// 		db.query(q, [values], (err, data) => {
-// 			if (err) return res.status(500).json(err);
-// 			return res.status(200).json("User created successfully");
-// 		});
-// 	});
-// };
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
 	// console.log("Request body:", req.body);
@@ -60,6 +37,25 @@ export const login = (req, res) => {
 	db.query(q, [req.body.username], (err, data) => {
 		if (err) return res.json(err);
 		if (data.length === 0) return res.status(404).json("User not found");
+
+		//Check the hash password
+
+		const isPasswordCorrect = bcrypt.compareSync(
+			req.body.password,
+			data[0].password
+		);
+		if (!isPasswordCorrect)
+			return res.status(400).json("Wrong username or password");
+
+		const token = jwt.sign({ id: data[0].id }, "jwtkey");
+		const { password, ...other } = data[0];
+
+		res
+			.cookie("access_token", token, {
+				httpOnly: true,
+			})
+			.status(200)
+			.json(other);
 	});
 };
 
--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@ import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
